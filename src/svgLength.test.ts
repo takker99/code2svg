79bccac8547e4220105a16847eae00fd37420c12
takker99 <37929109+takker99@users.noreply.github.com>
2023-02-19 18:53:34 +0900

    :white_check_mark: Add test cases of `parseSVGLength`

--- src/svgLength.test.ts
+++ src/svgLength.test.ts
@@ -1,13 +1,35 @@
 import { parseSVGLength } from "./svgLength.ts";
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 
-Deno.test("parseSVGLength()", () => {
-  assertEquals(parseSVGLength("400"), { length: 400, unit: "px" });
-  assertEquals(parseSVGLength("400px"), { length: 400, unit: "px" });
-  assertEquals(parseSVGLength("40.0"), { length: 40.0, unit: "px" });
-  assertEquals(parseSVGLength("40.0px"), { length: 40.0, unit: "px" });
-  assertEquals(parseSVGLength("40."), { length: 40, unit: "px" });
-  assertEquals(parseSVGLength("40."), { length: 40, unit: "px" });
-  assertEquals(parseSVGLength(".43"), { length: .43, unit: "px" });
-  assertEquals(parseSVGLength(".43"), { length: .43, unit: "px" });
+Deno.test("parseSVGLength()", async (t) => {
+  await t.step("must pass", async (t) => {
+    const units = [
+      "em",
+      "ex",
+      "px",
+      "in",
+      "cm",
+      "mm",
+      "pt",
+      "pc",
+      "%",
+      "",
+    ] as const;
+    for (const unit of units) {
+      for (const length of ["400", "40.0", ".43", "40."]) {
+        const str = `${length}${unit}`;
+        await t.step(str, () => {
+          assertEquals(parseSVGLength(str), {
+            length: parseFloat(length),
+            unit: unit || "px",
+          });
+        });
+      }
+    }
+  });
+
+  await t.step("cannot pass", () => {
+    assertEquals(parseSVGLength("500ch"), undefined);
+    assertEquals(parseSVGLength("hello"), undefined);
+  });
 });
